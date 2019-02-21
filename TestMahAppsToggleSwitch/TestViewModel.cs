61f20e2596c72a1e42c6858dbf9bc7dea0015853
Domas Markevicius <domas.mark@gmail.com>
2019-02-21 22:42:06 +0200

    Initial

---
+++ TestMahAppsToggleSwitch/TestViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMahAppsToggleSwitch {
+    public class TestViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged ([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (propertyName));
+        }
+
+
+
+        private bool _IsA { get; set; }
+        public bool IsA { get { return _IsA; } set { MaybeSetA (value); } }
+
+        private void MaybeSetA (bool value) {
+            var rand = new Random ();
+            if (rand.NextDouble () > 0.5) {
+                Trace.WriteLine ("set new value");
+                _IsA = value;
+            } else {
+                Trace.WriteLine ("ignore new value");
+            }
+        }
+
+    }
+}
